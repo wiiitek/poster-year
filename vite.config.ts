36e62c2794e602c93073a1b9b2cbca4637dff566
Witek <wiiitek>
2026-03-31 22:30:25 +0200

    Initial Vite config

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  server: {
+    port: 8765,
+    open: false,
+  },
+})
