9230d70448b6bc220ef1b715153f45d5a20ffc57
Witek <wiiitek>
2026-06-15 22:58:54 +0200

    Simple parts

--- src/chart/gradientColorsPlugin/arrayInterpolation.ts
+++ src/chart/gradientColorsPlugin/arrayInterpolation.ts
@@ -1,8 +1,18 @@
 
-export function splitIntoParts(input: (string | null)[]): (string | number)[][] {
-    const parts: (string | number)[][] = []
+export function splitIntoParts(input: (string | null)[]): (string | null)[][] {
+    const parts: (string | null)[][] = []
 
-    
+    const nonNullItemsAt = []
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] !== null) {
+            nonNullItemsAt.push(i)
+        }
+    }
+    for (let i = 0; i < nonNullItemsAt.length - 1; i++) {
+        const start = nonNullItemsAt[i]
+        const end = nonNullItemsAt[i + 1]
+        parts.push(input.slice(start, end + 1))
+    }
 
     return parts
 }
