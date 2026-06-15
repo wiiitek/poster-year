9e6fd3aef9557be5f700414e9dce2a07fc79be4a
Witek <wiiitek>
2026-06-15 23:14:15 +0200

    Indent of 2 spaces

--- src/chart/gradientColorsPlugin/arrayInterpolation.test.ts
+++ src/chart/gradientColorsPlugin/arrayInterpolation.test.ts
@@ -2,77 +2,77 @@ import { describe, it, expect } from 'vitest'
 import { interpolateArray, splitIntoParts } from './arrayInterpolation'
 
 function dummyInterpolation(start: string, end: string, steps: number): string[] {
-    const startNum = parseInt(start)
-    const endNum = parseInt(end)
-    const stepSize = (endNum - startNum) / (steps - 1)
+  const startNum = parseInt(start)
+  const endNum = parseInt(end)
+  const stepSize = (endNum - startNum) / (steps - 1)
 
-    const result: string[] = []
-    for (let i = 0; i < steps; i++) {
-        const stepValue = Math.round(startNum + stepSize * i)
-        result.push(stepValue.toString())
-    }
-    return result
+  const result: string[] = []
+  for (let i = 0; i < steps; i++) {
+    const stepValue = Math.round(startNum + stepSize * i)
+    result.push(stepValue.toString())
+  }
+  return result
 }
 
 describe('arrayInterpolation', () => {
 
-    describe('dummyInterpolation', () => {
+  describe('dummyInterpolation', () => {
 
-        const actual = dummyInterpolation('0', '8', 5)
+    const actual = dummyInterpolation('0', '8', 5)
 
-        it('should return an array of 5 elements', () => {
-            expect(actual).toHaveLength(5)
-        })
-
-        it('should return an array of 5 elements', () => {
-            expect(actual).toStrictEqual(['0', '2', '4', '6', '8'])
-        })
+    it('should return an array of 5 elements', () => {
+      expect(actual).toHaveLength(5)
     })
 
-    describe('splitIntoParts', () => {
+    it('should return an array of 5 elements', () => {
+      expect(actual).toStrictEqual(['0', '2', '4', '6', '8'])
+    })
+  })
 
-        it('should return parts when no nulls', () => {
-            const actual = splitIntoParts(['0', '70', '80'])
+  describe('splitIntoParts', () => {
 
-            expect(actual).toStrictEqual([
-                { start: 0, end: 1, startValue: '0', endValue: '70' },
-                { start: 1, end: 2, startValue: '70', endValue: '80' },
-                { start: 2, end: 0, startValue: '80', endValue: '0' }
-            ])
-        })
+    it('should return parts when no nulls', () => {
+      const actual = splitIntoParts(['0', '70', '80'])
 
-        it('should split simple array into parts', () => {
-            const actual = splitIntoParts(['0', null, '70', null, '80'])
-
-            expect(actual).toStrictEqual([
-                { start: 0, end: 2, startValue: '0', endValue: '70' },
-                { start: 2, end: 4, startValue: '70', endValue: '80' },
-                { start: 4, end: 0, startValue: '80', endValue: '0' }
-            ])
-        })
-
-        it('should split array into parts when values inside', () => {
-            const actual = splitIntoParts([ null, '70', null, '80', null])
-
-            expect(actual).toStrictEqual([
-                { start: 1, end: 3, startValue: '70', endValue: '80' },
-                { start: 3, end: 1, startValue: '80', endValue: '70' }
-            ])
-        })
+      expect(actual).toStrictEqual([
+        { start: 0, end: 1, startValue: '0', endValue: '70' },
+        { start: 1, end: 2, startValue: '70', endValue: '80' },
+        { start: 2, end: 0, startValue: '80', endValue: '0' }
+      ])
     })
 
-    describe('interpolateArray', () => {
+    it('should split simple array into parts', () => {
+      const actual = splitIntoParts(['0', null, '70', null, '80'])
 
-        it('should interpolate simple array', () => {
-            const actual = interpolateArray(['0', null, null, null, '8'], dummyInterpolation)
-
-            expect(actual).toStrictEqual(['0', '2', '4', '6', '8'])
-        })
-
-        it('should interpolate array with two fixed items', () => {
-            const actual = interpolateArray(['0', null, '70', null, '80'], dummyInterpolation)
-
-            expect(actual).toStrictEqual(['0', '35', '70', '75', '80'])
-        })
+      expect(actual).toStrictEqual([
+        { start: 0, end: 2, startValue: '0', endValue: '70' },
+        { start: 2, end: 4, startValue: '70', endValue: '80' },
+        { start: 4, end: 0, startValue: '80', endValue: '0' }
+      ])
     })
+
+    it('should split array into parts when values inside', () => {
+      const actual = splitIntoParts([ null, '70', null, '80', null])
+
+      expect(actual).toStrictEqual([
+        { start: 1, end: 3, startValue: '70', endValue: '80' },
+        { start: 3, end: 1, startValue: '80', endValue: '70' }
+      ])
+    })
+  })
+
+  describe('interpolateArray', () => {
+
+    it('should interpolate simple array', () => {
+      const actual = interpolateArray(['0', null, null, null, '8'], dummyInterpolation)
+
+      expect(actual).toStrictEqual(['0', '2', '4', '6', '8'])
+    })
+
+    it('should interpolate array with two fixed items', () => {
+      const actual = interpolateArray(['0', null, '70', null, '80'], dummyInterpolation)
+
+      expect(actual).toStrictEqual(['0', '35', '70', '75', '80'])
+    })
+  })
 })
--- src/chart/gradientColorsPlugin/arrayInterpolation.ts
+++ src/chart/gradientColorsPlugin/arrayInterpolation.ts
@@ -1,44 +1,46 @@
 
 export interface Part {
-    start: number
-    end: number
-    startValue: string
-    endValue: string
+  start: number
+  end: number
+  startValue: string
+  endValue: string
 }
 
 export function splitIntoParts(input: (string | null)[]): Part[] {
-    const parts: Part[] = []
+  const parts: Part[] = []
 
-    const nonNullItemsAt: number[] = []
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] !== null) {
-            nonNullItemsAt.push(i)
-        }
+  const nonNullItemsAt: number[] = []
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== null) {
+      nonNullItemsAt.push(i)
     }
-    for (let i = 0; i < nonNullItemsAt.length - 1; i++) {
-        const start = nonNullItemsAt[i]
-        const end = nonNullItemsAt[i + 1]
-        const startValue = input[start]!!
-        const endValue = input[end]!!
-        parts.push({ start, end, startValue, endValue })
-    }
-    // and adds additional part to close cycle
-    const first = parts[0]
-    const last = parts[parts.length - 1]
-    const connectingPart = {
-        start: last.end,
-        end: first.start,
-        startValue: last.endValue,
-        endValue: first.startValue
-    }
-    parts.push(connectingPart)
+  }
+  for (let i = 0; i < nonNullItemsAt.length - 1; i++) {
+    const start = nonNullItemsAt[i]
+    const end = nonNullItemsAt[i + 1]
+    const startValue = input[start]!!
+    const endValue = input[end]!!
+    parts.push({ start, end, startValue, endValue })
+  }
+  // and adds additional part to close cycle
+  const first = parts[0]
+  const last = parts[parts.length - 1]
+  const connectingPart = {
+    start: last.end,
+    end: first.start,
+    startValue: last.endValue,
+    endValue: first.startValue
+  }
+  parts.push(connectingPart)
 
-    return parts
+  return parts
 }
 
 export function interpolateArray(
-    input: (string | null)[],
-    interpolation: (start: string, end: string, steps: number) => string[]
+  input: (string | null)[],
+  interpolation: (start: string, end: string, steps: number) => string[]
 ): string[] {
-    return interpolation(input[0]!!, input[input.length - 1]!!, input.length)
+  const parts: Part[] = splitIntoParts(input)
+
+  return interpolation(input[0]!!, input[input.length - 1]!!, input.length)
 }
